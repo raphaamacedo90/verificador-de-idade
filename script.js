6bed61f01b4d3682223f762ac48e64ae7bb8d5e9
Raphael Macedo <raphaa.macedo@hotmail.com>
2021-06-04 09:28:15 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,72 @@
+
+
+
+function verificar(){
+    const data  = new Date()
+    const ano = data.getFullYear()
+    const txtano = document.getElementById("txtano")
+    const result = document.getElementById("resultado")
+
+     
+
+    
+
+    if(txtano.value <= 1900 || Number(txtano.value) >= ano){
+
+    alert("[ERRO] Verifique os dados e tente novamente! ")
+    }else{
+        const sexo = document.getElementsByName("sexo")
+        const idade = ano - Number(txtano.value)
+        let genero = ""
+        let img = document.createElement("img")
+        img.setAttribute("id", "foto")
+
+        if(sexo[0].checked){
+            genero = "Homem"
+            if(idade >= 0 && idade < 12){
+                //criança
+                img.setAttribute("src", "bebem.png")
+                
+            }else if (idade <= 30){
+                //jovem
+                img.setAttribute("src", "jovemm.png")
+
+            }else if (idade < 50){
+                //adulto
+                img.setAttribute("src", "adultom.png")
+
+            }else{
+                //idoso
+                img.setAttribute("src", "idosom.png")
+
+            }
+
+
+        }else if(sexo[1].checked){
+            genero = "Mulher"
+            if(idade >= 0 && idade < 12){
+                //criança
+                img.setAttribute("src", "bebef.png")
+                
+            }else if (idade <= 30){
+                //jovem
+                img.setAttribute("src", "jovemf.png")
+
+            }else if (idade < 50){
+                //adulto
+                img.setAttribute("src", "adultof.png")
+
+            }else{
+                //idoso
+                img.setAttribute("src", "idosof.png")
+
+            }
+        }
+        result.style.textAlign = "center"
+        result.innerHTML = `Detectamos um ${genero} e tem ${idade} anos.`
+        result.appendChild(img)
+        
+        
+    }
+  
+}    
